16589ae4499f2d1e99422960bc9a6bd24a27ec6b
penghui <penghuilater@gmail.com>
2016-07-03 15:53:12 +0800

    Query select where

--- database/query_sqlite3.py
+++ database/query_sqlite3.py
@@ -10,6 +10,19 @@ def select_distinct():
     cur.execute('select distinct name from user')
     values = cur.fetchall()
     print("name column:", values)
+    cur.close()
+    connect.close()
+
+def select_where():
+    con = sqlite3.connect('sqlite3_demo.db')
+    cur = con.cursor()
+    cur.execute('select * from user where id == 10 and name == \'Peter\'')
+    values = cur.fetchall()
+    print values
+    cur.close()
+    con.close()
 
 if __name__ == '__main__':
     select_distinct()
+    print('SELECT WHERE')
+    select_where()
