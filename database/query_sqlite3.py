40f950f99bc1b3ba9254cf351e5085d1e92b0b63
penghui <penghuilater@gmail.com>
2016-07-03 16:34:56 +0800

    Add select order by

--- database/query_sqlite3.py
+++ database/query_sqlite3.py
@@ -19,10 +19,25 @@ def select_where():
     cur.execute('select * from user where id == 10 and name == \'Peter\'')
     values = cur.fetchall()
     print values
+
+    cur.execute('select * from user where name == \'Green\' or name == \'Peter\'')
+    values = cur.fetchall()
+    print values
     cur.close()
+
     con.close()
+
+def query_order():
+    con = sqlite3.connect('sqlite3_demo.db')
+    cur = con.cursor()
+    cur.execute('select name from user order by name')
+    values = cur.fetchall()
+    print("query by orders:\n")
+    print values
 
 if __name__ == '__main__':
     select_distinct()
     print('SELECT WHERE')
     select_where()
+    query_order()
+
