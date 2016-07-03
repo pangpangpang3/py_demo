b4f503da0368f319305b040a529ebef1c7f8892f
penghui <penghuilater@gmail.com>
2016-07-03 20:45:20 +0800

    Add like,reg database demo

--- database/like_sqlite3.py
+++ database/like_sqlite3.py
@@ -9,6 +9,15 @@ def like():
     cur.close()
     con.close()
 
+def reg():
+    con = sqlite3.connect('sqlite3_demo.db')
+    cur = con.cursor()
+    cur.execute('select * from user where name like \'T_m\'')
+    values = cur.fetchall()
+    print("reg:/n")
+    print values
+    cur.close()
+    con.close()
 
 if __name__ == '__main__':
     like()
